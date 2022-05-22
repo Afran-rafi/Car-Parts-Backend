0f4870d09ec19a78df2ef96d6500bd0326e42f03
Afran-rafi <fahimrrrrr@gmail.com>
2022-05-22 09:34:21 +0600

    user email JWT done

--- index.js
+++ index.js
@@ -34,17 +34,7 @@ async function run() {
         await client.connect()
         const partsCollection = client.db("Assignment_Twelve").collection("CarParts")
         const OrderCollection = client.db("Assignment_Twelve").collection("Order")
-
-        const verifyAdmin = async (req, res, next) => {
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
-                next();
-            }
-            else {
-                res.status(403).send({ message: 'forbidden' });
-            }
-        }
+        const userCollection = client.db("Assignment_Twelve").collection("User")
 
         app.get('/carParts', async (req, res) => {
             const query = {};
@@ -66,6 +56,18 @@ async function run() {
             res.send(result);
         });
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, option)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
     }
     finally {
 
